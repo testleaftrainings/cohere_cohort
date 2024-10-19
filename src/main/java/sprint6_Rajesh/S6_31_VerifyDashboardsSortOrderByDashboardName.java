2a1077e15c24d4fbea9612dc3895cb56432ec55c
Rajeshluffy <rajeshveeramani12@gmail.com>
2024-10-19 22:09:35 +0530

    adding new file

---
+++ src/main/java/sprint6_Rajesh/S6_31_VerifyDashboardsSortOrderByDashboardName.java
@@ -0,0 +1,91 @@
+package sprint6_Rajesh;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class S6_31_VerifyDashboardsSortOrderByDashboardName extends BaseClass {
+
+	public S6_31_VerifyDashboardsSortOrderByDashboardName(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+
+	}
+
+
+	public void s6_031_verifyDashboardsSortOrderByDashboardName() throws InterruptedException {
+
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+		//		Click on the toggle menu button from the left corner
+
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button[@title='App Launcher']")))).click();
+
+
+		// Click View All and click Dashboards from App Launcher
+
+		Thread.sleep(2000);
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//button[text()='View All']")))).click();
+
+
+		WebElement dashboarEle = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[text()='All Items']//following::p[text()='Dashboards']")))); 
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();",dashboarEle);
+
+		//		Click on the Dashboards tab 
+
+		dashboarEle.click();
+
+		//		Click the sort arrow in the Dashboard Name.
+		Thread.sleep(2000);
+
+		List<WebElement> rawNameListEle= driver.findElements(By.xpath("//table//following::tr/th"));
+		List<String> rawNameList = getAllText(rawNameListEle);
+
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("(//div[@title='New Dashboard']//following::thead//a)[1]//span[@title='Dashboard Name']")))).click();
+
+		Thread.sleep(5000);
+		List<WebElement> sortNameListEle= driver.findElements(By.xpath("//table//following::tr/th"));
+
+
+		//		Verify the Dashboard displayed in ascending order by Dashboard name.
+
+		List<String> sortNameList = getAllText(sortNameListEle);
+
+
+		if (!rawNameList.equals(sortNameList)) {
+			Collections.sort(rawNameList,String.CASE_INSENSITIVE_ORDER);
+
+			if (rawNameList.equals(sortNameList)) {
+				org.testng.Assert.assertTrue(true);
+
+			}else {
+				org.testng.Assert.assertTrue(false);
+			}
+		} else {
+			org.testng.Assert.assertTrue(false);
+		}
+
+	}
+
+
+	public  List<String> getAllText(List<WebElement> rawNameListEle) {
+
+		List<String> namesList = new ArrayList<>();
+		for (WebElement element : rawNameListEle) {
+			namesList.add(element.getText());  // Get text and add to namesList
+		}
+		return namesList;
+	}
+
+
+}
---
+++ src/main/java/sprint6_Rajesh/S6_37_VerifyAppStoreLoadedToDownloadSalesForceAStory.java
@@ -0,0 +1,128 @@
+package sprint6_Rajesh;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.github.sukgu.Shadow;
+
+public class S6_37_VerifyAppStoreLoadedToDownloadSalesForceAStory extends BaseClass{
+
+	public S6_37_VerifyAppStoreLoadedToDownloadSalesForceAStory(WebDriver driver) {
+		this.driver=driver;
+	}
+	@Test
+	public void VerifyAppStore() throws InterruptedException {
+		
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+		//		Click on the sliding icon until """"Get started with Einstein bots"""" is displayed
+
+		Thread.sleep(5000);
+		List<WebElement> labelEle = driver.findElements(By.xpath("(//div[@id='setupComponent']//following::h2/span)"));
+
+		int count = 1;
+		for (int i = 0; i < labelEle.size(); i++) {
+			int temp =i+1;
+			WebElement label = driver.findElement(By.xpath("(//div[@id='setupComponent']//following::h2/span)["+temp+"]"));
+			String labelNa = label.getText();
+
+			if (labelNa.equalsIgnoreCase("Get Started with Einstein Bots")) {
+
+
+				//				Click on Get Started link
+				Thread.sleep(5000);
+				WebElement getStart = driver.findElement(By.xpath("(//button[@title='Get Started'])["+count+"]"));
+				getStart.click();
+
+
+				break;
+
+			}else if(count ==3){
+				count =1;
+				driver.findElement(By.xpath("//div[@class='leftScroll']//following::button[1]")).click();
+				break;
+			}
+			count ++;
+		}
+
+
+		//		Click on the sliding icon until """"Einstein Bot Trails"""" is displayed  && Click on the Einstein Bot Trails.
+
+		Thread.sleep(5000);
+		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//div[@id='setupComponent']//following::h2[text()='Einstein Bots Trails']")))).click();
+
+
+		Windowhandle(driver, 1);
+
+
+		Shadow shadow = new Shadow(driver);
+
+
+
+		Thread.sleep(2000);
+		//scroll the element and click
+		WebElement trail = shadow.findElementByXPath("//span[text()='Products']");
+		//		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();",trail);
+		trail.click();
+
+
+		// Click on the 'Partner Apps and Experts'
+
+		WebElement partner = shadow.findElementByXPath("//div[@id='global-nav-container']//following::span[text()='Partner Apps & Experts']");
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();",partner);
+		partner.click();
+
+
+		//	Click on the Find an app'
+
+		driver.findElement(By.xpath("(//main[@id='main-content']//following::a)[2]")).click();
+
+		//		Search for 'SalesforceA
+
+		shadow.findElementByXPath("//input[@title='Search AppExchange']").sendKeys("SalesforceA");
+		Thread.sleep(2000);
+		shadow.findElementByXPath("//div[@data-item='SalesforceA']").click();
+
+
+		//		Click on the 'SalesforceA Connected Apps Administration'
+		Thread.sleep(2000);
+		shadow.findElementByXPath("//a[text()='SalesforceA Connected Apps Administration']").click();
+
+		//		Verify  'SalesforceA Connected Apps Administration' is displayed with Get It Now button.
+
+		Thread.sleep(2000);
+
+		boolean title = shadow.findElementByXPath("//h1[text()='SalesforceA Connected Apps Administration']").isDisplayed();
+		boolean button = shadow.findElementByXPath("//button[@class='element variant-primary']").isDisplayed();
+		if (title&&button) {
+			Assert.assertTrue(title);
+		}else {
+			Assert.assertTrue(false);
+		}
+		driver.quit();
+	}
+
+	public void Windowhandle(WebDriver driver,int a) {
+
+		Set<String> allWindowHandles = driver.getWindowHandles();
+
+		List <String> window =new ArrayList<>(allWindowHandles);
+
+		driver.switchTo().window(window.get(a));
+		allWindowHandles.clear();
+		window.clear();
+	}
+
+}
