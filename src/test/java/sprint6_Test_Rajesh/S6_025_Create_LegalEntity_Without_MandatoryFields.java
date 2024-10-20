30be103ac85cfc3d96d5faf1354f39f2cbe9f08a
Rajeshluffy <rajeshveeramani12@gmail.com>
2024-10-20 17:43:00 +0530

    test folder

---
+++ src/test/java/sprint6_Test_Rajesh/S6_025_Create_LegalEntity_Without_MandatoryFields.java
@@ -0,0 +1,25 @@
+package sprint6_Test_Rajesh;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import sprint6_Rajesh.*;
+
+public class S6_025_Create_LegalEntity_Without_MandatoryFields  extends BaseClass {
+	@BeforeMethod
+	public void browser() {
+		browserInitiate("chrome");
+	}
+	@Test(retryAnalyzer = RetryEvent.class)
+	public void testCase_S6_025_createLegalEntityWithoutMandatoryFields() throws InterruptedException {
+		browserInitiate("chrome");
+
+        SaleforceLogin login = new SaleforceLogin(driver);  
+        login.saleForceLogin();
+        
+        S6_25_CreateLegalEntityWithoutMandatoryFields cLEWMF=   new S6_25_CreateLegalEntityWithoutMandatoryFields(driver);
+       
+        cLEWMF.s6_025_createLegalEntityWithoutMandatoryFields(); 
+	}
+	
+}
---
+++ src/test/java/sprint6_Test_Rajesh/S6_053_AddProductWithOpportunity.java
@@ -0,0 +1,23 @@
+package sprint6_Test_Rajesh;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import sprint6_Rajesh.*;
+
+public class S6_053_AddProductWithOpportunity  extends BaseClass {
+	@BeforeMethod
+	public void browser() {
+		browserInitiate("chrome");
+	}
+
+	@Test(retryAnalyzer = RetryEvent.class)
+	public void testCase_S6_053_verify_AddProductWithOpportunity() throws InterruptedException {
+
+		SaleforceLogin login = new SaleforceLogin(driver);  
+		login.saleForceLogin();
+		S6_53_AddProductWithOpportunity aPWO = new S6_53_AddProductWithOpportunity(driver);
+		aPWO.addProduct();
+	}
+
+}
