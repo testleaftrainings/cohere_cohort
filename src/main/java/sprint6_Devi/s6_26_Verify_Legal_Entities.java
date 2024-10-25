e177f588da480da6f5ecbb719ce6d7ba1882ac1d
Rajeshluffy <rajeshveeramani12@gmail.com>
2024-10-25 22:19:49 +0530

    jira-ticket

--- src/main/java/sprint6_Devi/s6_26_Verify_Legal_Entities.java
+++ src/main/java/sprint6_Devi/s6_26_Verify_Legal_Entities.java
@@ -1,4 +1,6 @@
 package sprint6_Devi;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -8,22 +10,34 @@ import java.util.Date;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import sprint6_Rajesh.RetryEvent;
 
 public class s6_26_Verify_Legal_Entities {
 	@Test(retryAnalyzer = RetryEvent.class)
-	public void verifyLegalEntities() throws ParseException, InterruptedException {
+	public void verifyLegalEntities() throws ParseException, InterruptedException, MalformedURLException {
 		EdgeOptions edgeOptions = new EdgeOptions();
 		edgeOptions.addArguments("--disable-notifications");
-		EdgeDriver driver = new EdgeDriver(edgeOptions);
+        
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setBrowserName("MicrosoftEdge");
+        dc.setPlatform(Platform.LINUX);
+        dc.setCapability(ChromeOptions.CAPABILITY, edgeOptions);
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 		//Step 1: Launch the salesforce app
 		driver.get("https://login.salesforce.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
