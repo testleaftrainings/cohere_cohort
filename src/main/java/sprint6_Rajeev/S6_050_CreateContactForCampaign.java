188559ce402d0d3880d869c04ddc187216212d00
Rajeshluffy <rajeshveeramani12@gmail.com>
2024-10-25 22:39:35 +0530

    jira

--- src/main/java/sprint6_Rajeev/S6_050_CreateContactForCampaign.java
+++ src/main/java/sprint6_Rajeev/S6_050_CreateContactForCampaign.java
@@ -1,6 +1,5 @@
 package sprint6_Rajeev;
 
-import java.awt.Window;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -9,12 +8,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class S6_050_CreateContactForCampaign {
@@ -30,14 +28,14 @@ public class S6_050_CreateContactForCampaign {
 
 		
 		// TODO Auto-generated method stub
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
-		//ChromeDriver driver = new ChromeDriver(options);
+		EdgeOptions edgeOptions = new EdgeOptions();
+		edgeOptions.addArguments("--disable-notifications");
+
 		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setCapability(EdgeOptions.CAPABILITY, edgeOptions); // Use EdgeOptions.CAPABILITY for Edge
 		dc.setBrowserName("MicrosoftEdge");
 		dc.setPlatform(Platform.LINUX);
-		dc.setCapability(ChromeOptions.CAPABILITY, options);
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
