3e9b4d0801ae1eaba1192094ff73256e3f25266f
rajeev991x <rajeev991x@gmail.com>
2024-10-26 00:21:00 +0800

    update testcase - jenkins

--- src/main/java/sprint6_Rajeev/OpportunitiesSortOrderByCloseDate.java
+++ src/main/java/sprint6_Rajeev/OpportunitiesSortOrderByCloseDate.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,15 +21,17 @@ public class OpportunitiesSortOrderByCloseDate {
 	public void createAccount() throws InterruptedException
 	{
 
-		ChromeDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--disable-notifications");
+		ChromeDriver driver = new ChromeDriver(chromeOptions);
 		driver.get("https://login.salesforce.com/");
-		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30000));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.manage().window().maximize();
 		
 		//Step 1: Login to Login | Salesforce 
 		driver.findElement(By.id("username")).sendKeys("gokul.sekar@testleaf.com");
 		Thread.sleep(2000);
-		driver.findElement(By.id("password")).sendKeys("Leaf$321");
+		driver.findElement(By.id("password")).sendKeys("Leaf@123");
 		Thread.sleep(2000);
 		driver.findElement(By.id("Login")).click();
 		Thread.sleep(2000);
