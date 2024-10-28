0e84d2b51654a7225216989c22eb2cb55b90eea8
Gokulakrishnan2208 <mgokulakrishnan02@gmail.com>
2024-10-28 10:36:30 +0530

    2810

--- src/main/java/sprint6_Gokul/NbaPlayerExp.java
+++ src/main/java/sprint6_Gokul/NbaPlayerExp.java
@@ -1,4 +1,4 @@
-package PackHasSingleClass;
+package sprint6_Gokul;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -21,6 +21,7 @@ import org.testng.asserts.SoftAssert;
 
 public class NbaPlayerExp {
 	static RemoteWebDriver driver;
+
 	@Test
 	public void playerExp() throws InterruptedException, MalformedURLException {
 //		DesiredCapabilities dc = new DesiredCapabilities();
@@ -28,13 +29,13 @@ public class NbaPlayerExp {
 ////		dc.setBrowserName("Chrome");
 //        dc.setPlatform(Platform.LINUX);
 //        driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
-		
-		ChromeDriver driver=new ChromeDriver();
+
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(" https://www.nba.com/stats");
 		List<String> lin = new ArrayList<String>();
 		List<Integer> lia = new ArrayList<Integer>();
-		Map<Integer,String> m = new HashMap<Integer,String>();
+		Map<Integer, String> m = new HashMap<Integer, String>();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//div//button[text()='I Accept']")).click();
 		driver.findElement(By.linkText("See All Player Stats")).click();
@@ -49,52 +50,50 @@ public class NbaPlayerExp {
 
 		Thread.sleep(5000);
 		try {
-		WebElement dialog = driver.findElement(By.xpath("(//div[@role='dialog']//button[@data-click='close']//span)[1]"));
+			WebElement dialog = driver
+					.findElement(By.xpath("(//div[@role='dialog']//button[@data-click='close']//span)[1]"));
 //		action.moveToElement(dialog);
-		dialog.click();
+			dialog.click();
+		} catch (Exception e) {
+
 		}
-		catch(Exception e)
-		{
-			
-		}
-		
-		List<WebElement> names = driver.findElements(By.xpath("//div//table//tr//th[text()='Player']//following::tr//td//following::td[contains(@class,'Crom_primary')]//a"));
-		
-		for(WebElement name:names)
-		{
+
+		List<WebElement> names = driver.findElements(By.xpath(
+				"//div//table//tr//th[text()='Player']//following::tr//td//following::td[contains(@class,'Crom_primary')]//a"));
+
+		for (WebElement name : names) {
 			String nm = name.getText();
-			System.out.println(nm);
+//			System.out.println(nm);
 			lin.add(nm);
 		}
-		
-List<WebElement> ages = driver.findElements(By.xpath("//div//table//tr//th[text()='Player']//following::tr//td[contains(@class,'Crom_stickyRank')]//following::td[3]"));
-		
-		for(WebElement age:ages)
-		{
+
+		List<WebElement> ages = driver.findElements(By.xpath(
+				"//div//table//tr//th[text()='Player']//following::tr//td[contains(@class,'Crom_stickyRank')]//following::td[3]"));
+
+		for (WebElement age : ages) {
 			String ag = age.getText();
 			int a = Integer.parseInt(ag);
-			System.out.println(ag);
+//			System.out.println(ag);
 			lia.add(a);
 		}
-		
-		for(int i=0;i<lin.size();i++)
-		{
-			String n1= lin.get(i);
-			int a1=lia.get(i);
+
+		for (int i = 0; i < lin.size(); i++) {
+			String n1 = lin.get(i);
+			int a1 = lia.get(i);
 			m.put(a1, n1);
 		}
 		Collections.sort(lia);
-		String str=m.get(lia.get(0));
-		
+		String str = m.get(lia.get(0));
+
 		System.out.println(str);
 		driver.findElement(By.xpath("//button[@title='Add a custom filter']")).click();
 		driver.findElement(By.xpath("//input[@placeholder='Value']")).sendKeys(str);
 		driver.findElement(By.linkText(str)).click();
 		Thread.sleep(1000);
-		WebElement exp =driver.findElement(By.xpath("//div//p[text()='EXPERIENCE']//following::p"));
-		String ex=exp.getText();
+		WebElement exp = driver.findElement(By.xpath("//div//p[text()='EXPERIENCE']//following::p"));
+		String ex = exp.getText();
 		SoftAssert sassert = new SoftAssert();
-		sassert.assertEquals(ex, "1 Year","Not equal");
+		sassert.assertEquals(ex, "1 Year", "Not equal");
 		sassert.assertAll();
 		driver.quit();
 	}
